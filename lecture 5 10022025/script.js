498f1a4e2fe871d44f4aba7464ede3377332f961
Harshit-0007 <harshit1716.be21@chitkara.edu.in>
2025-02-10 15:40:29 +0530

    Lecture 5 10/02/2025

---
+++ lecture 5 10022025/script.js
@@ -0,0 +1,18 @@
+
+var a=10;
+function squareoftwoNumber(x){
+    var ans=x*x;
+    return ans;
+}
+console.log(squareoftwoNumber(a));
+// it Create GEC(global execution context)
+// And taught hosting
+// types of error
+// 1. ReferenceError
+// 2. TypeError
+// 3. SyntaxError
+
+// lexical order or lexical scope
+// it has child parent grandparent
+//difference between undefined vs not defined
+// what is TDZ?
