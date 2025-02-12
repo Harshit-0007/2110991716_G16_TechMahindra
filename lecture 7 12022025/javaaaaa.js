29c6d8920c96e6a854e5e97a78d31cb85ccd4e70
Harshit-0007 <harshit1716.be21@chitkara.edu.in>
2025-02-12 15:36:29 +0530

    Lecture 7 12_feb2025

---
+++ lecture 7 12022025/javaaaaa.js
@@ -0,0 +1,28 @@
+// closure with example
+// console.log(1);
+// console.log(sumoftwo(10,5));
+// function sumoftwo(a,b){
+//     return a+b;
+// }
+// setTimeout(()=>{
+//     console.log(4)},2000);
+// var start=1;
+// var end=10000;
+// while(start<end){
+//     start++;
+// }
+// console.log('end');
+// setInterval(()=>{//it is a call back feature it make it function
+//     console.log(9)},5000);
+// ===================x==============x===========x=============x================
+
+// function xyz(){
+//     console.log(1);
+// }
+// var abc=function cdf(){
+//     console.log(2);
+// }
+// xyz();
+// cdf();
+// what is garbage collector in js 
+
