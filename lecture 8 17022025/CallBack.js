5561d59f6f70dc97b96c4ff29ee6bf4eed1b6b27
Harshit-0007 <harshit1716.be21@chitkara.edu.in>
2025-02-17 12:46:16 +0530

    Lecture 8 17 feb callBack

---
+++ lecture 8 17022025/CallBack.js
@@ -0,0 +1,111 @@
+// callBack
+
+// function a(){
+//     console.log("firstttt");
+// }
+// function b(a){
+// //     console.log("secondttt");
+// //     a();
+// //     }
+// //     b(a);
+
+
+// document.getElementById("btn-click").addEventListener("click",function(){
+    
+//     console.log(count++);
+// })
+// document.getElementById("btn-click").addEventListener("click",function(){
+    
+//     console.log(count--);
+// })
+// function x(()=>{
+// //     let x=0;
+// //     return {
+// //         inc: function(){
+// //             console.log(count++);
+// //         }
+// //         dec: function(){
+// //             console.log(count--);
+// //         }
+// //     }
+// // }
+// // let y=x();
+// // ====================x==================x=================x===================x==================
+
+// function dhaba(callBack){
+//     console.log("enter dhaba");
+//     setTimeout(callBack,1000);
+// }
+// function table(callBack){
+//     console.log("table find");
+//     setTimeout(callBack,1000);
+// }
+// function menu(callBack){
+//     console.log("menu discover");
+//     setTimeout(callBack,1000);
+// }
+// function price(callBack){
+//     console.log("price check");
+//     setTimeout(callBack,1000);
+// }
+// function starter(callBack){
+//     console.log("starter order");
+//     setTimeout(callBack,1000);
+// }
+// function main(callBack){
+//     console.log("main course");
+//     setTimeout(callBack,1000);
+// }
+// function desert(callBack){
+//     console.log("desert");
+//     setTimeout(callBack,1000);
+// }
+// function bill(callBack){
+//     console.log("billing");
+//     setTimeout(callBack,1000);
+// }
+// function left(callBack){
+//     console.log("you left");
+//     setTimeout(callBack,1000);
+// }
+// // chaining the callBacks
+// // dhaba(function(){
+// //     table(function(){
+// //         menu(function(){
+// //             price(function(){
+// //                 starter(function(){
+// //                     main(function(){
+// //                         desert(function(){
+// //                             bill(function(){
+// //                                 left(function(){
+// //                                     console.log("all done");
+// //                                     });
+// //                                     });
+// //                                     });
+// //                                     });
+// //                                     });
+// //                                 });
+// //                             });
+// //                         });
+// //                     });
+// dhaba(()=>{
+//     table(()=>{
+//         menu(()=>{
+//             price(()=>{
+//                 starter(()=>{
+//                     main(()=>{
+//                         desert(()=>{
+//                             bill(()=>{
+//                                 left(()=>{
+//                                     console.log("all done");
+//                                     });
+//                             });
+//                             });
+//                     });
+//                 });
+//             });
+//         });
+//     });
+// });
+// pyramidal loop solution promises
+// ----------------x----------------------------x---------------------x---------------------------x--------------
